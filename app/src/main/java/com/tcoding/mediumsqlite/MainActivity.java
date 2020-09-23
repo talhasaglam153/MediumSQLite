1fc3a4d640c2cf5b1d9cdb09ce915612d5bc049f
Talha Saglam <talhasaglam153@gmail.com>
2020-09-23 13:02:40 +0300

    Recyclerview nesnesi eklendi

--- app/src/main/java/com/tcoding/mediumsqlite/MainActivity.java
+++ app/src/main/java/com/tcoding/mediumsqlite/MainActivity.java
@@ -3,12 +3,16 @@ package com.tcoding.mediumsqlite;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rv = findViewById(R.id.rv);
+
     }
 }
---
+++ app/src/main/java/com/tcoding/mediumsqlite/Veritabani.java
@@ -0,0 +1,4 @@
+package com.tcoding.mediumsqlite;
+
+public class Veritabani {
+}
